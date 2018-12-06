20027470c11bf86caa6c1227c0c631beb85a33ea
Vince Ross <vross1701@gmail.com>
2018-12-06 05:35:51 -0500

    js operate function and called math functions

---
+++ calculator.js
@@ -0,0 +1,39 @@
+
+
+
+
+function operate(x, operator, y) {
+  switch(operator) {
+    case '+':
+      add(x, y);
+      break;
+    case '-':
+      subtract(x, y);
+      break;
+    case '*':
+      multiply(x, y);
+      break;
+    case '/':
+      divide(x, y);
+      break;
+    default:
+      break;
+
+  }
+}
+
+function add(x, y) {
+	return x + y;	
+}
+
+function subtract(x, y) {
+	return x - y;	
+}
+
+function multiply(x, y) {
+	return x * y;	
+}
+
+function divide(x, y) {
+	return x / y;
+}
